04a7e19899423b2fc13608ad435b878ef5792faf
philip ross <philip@satchel.scot>
2019-03-10 11:15:01 +0000

    feat: add cp-pkgs script

---
+++ scripts/cp-pkgs.js
@@ -0,0 +1,92 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * @fileoverview Copies built assets from our build directory to each of their respective package's
+ * dist/ folder.
+ */
+
+const path = require('path');
+const fs = require('fs');
+const cpFile = require('cp-file');
+const toSlugCase = require('to-slug-case');
+const { spawnSync } = require('child_process');
+const { sync: globSync } = require('glob');
+
+const PKG_RE = /(?:vue\-material\-adapter)|(?:mcwv\.[a-zA-Z\-]+)/;
+
+const isValidCwd =
+  path.basename(process.cwd()) === 'vue-material-adapter' &&
+  fs.existsSync('packages') &&
+  fs.existsSync('build');
+
+if (!isValidCwd) {
+  console.error(
+    'Invalid CWD. Please ensure you are running this from the root of the repo, and that you have run `npm run dist`',
+  );
+  process.exit(1);
+}
+
+function cleanPkgDistDirs() {
+  const statuses = globSync('packages/*/dist').map(
+    distPath => spawnSync('rm', ['-r', distPath], { stdio: 'inherit' }).status,
+  );
+
+  if (statuses.find(status => status > 0)) {
+    console.error('Failed to clean package dist folders prior to copying');
+    process.exit(1);
+  }
+}
+
+function getAssetEntry(asset) {
+  // console.dir(path.parse(asset).name.match(PKG_RE));
+  const [entryName] = path.parse(asset).name.match(PKG_RE);
+  const [MDC, componentName] = entryName.split('.');
+  const dealingWithSubpackage = Boolean(MDC && componentName);
+  if (!dealingWithSubpackage) {
+    // console.log(`dealingWithSubpackage: ${dealingWithSubpackage}: entryName`);
+    return entryName;
+  }
+  return [MDC, toSlugCase(componentName)].join('-');
+}
+
+function cpAsset(asset) {
+  const assetPkg = path.join('packages', getAssetEntry(asset));
+  console.dir(assetPkg);
+  if (!fs.existsSync(assetPkg)) {
+    Promise.reject(
+      new Error(`Non-existent asset package path ${assetPkg} for ${asset}`),
+    );
+  }
+  const destDir = path.join(assetPkg, 'dist', path.basename(asset));
+  return cpFile(asset, destDir).then(() =>
+    console.log(`cp ${asset} -> ${destDir}`),
+  );
+}
+
+cleanPkgDistDirs();
+
+Promise.all(globSync('build/*.{css,js,map}').map(cpAsset)).catch(err => {
+  console.error(`Error encountered copying assets: ${err}`);
+  process.exit(1);
+});
