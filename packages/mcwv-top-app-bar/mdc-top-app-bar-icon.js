b6f79fb4b7389d09067fa3fc26bd3c2aef539b7b
philip ross <philip@satchel.scot>
2019-04-25 18:25:42 +0100

    fix: second attempt at fixing top app bar icon

--- packages/mcwv-top-app-bar/mdc-top-app-bar-icon.js
+++ packages/mcwv-top-app-bar/mdc-top-app-bar-icon.js
@@ -12,19 +12,50 @@ export default {
       navIcon,
     } = this;
     const slot = (scopedSlots.default && scopedSlots.default()) || [];
-    const nodes = slot.filter(({ tag }) => tag);
-    if (nodes.length !== 1) {
-      throw new Error('mdc-top-app-bar-icon expects exactly one child element');
-    }
+    const nodes = slot.map(vn => {
+      if (!vn.tag || !vn.componentOptions) {
+        const data = vn.data || (vn.data = {});
+        data.staticClass = `${
+          actionItem ? cssClasses.ACTION_ITEM : cssClasses.navIcon
+        } ${data.staticClass}`;
+        data.attrs = { ...data.attrs, ...attrs };
+        return vn;
+      }
 
-    const [v0] = nodes;
-    const data = v0.data || (v0.data = {});
-    const sc = [data.staticClass || ''];
+      const data = vn.data || {};
+      return createElement(
+        vn.componentOptions.tag,
+        {
+          class: {
+            [cssClasses.ACTION_ITEM]: actionItem,
+            [cssClasses.NAV_ICON]: navIcon,
+          },
+          props: {
+            ...vn.componentOptions.propsData,
+          },
+          attrs: {
+            ...data.attrs,
+          },
+          on: vn.componentOptions.listeners,
+        },
+        vn.componentOptions.children,
+      );
+    });
 
-    actionItem && sc.push(cssClasses.ACTION_ITEM);
-    navIcon && sc.push(cssClasses.NAV_ICON);
-    v0.data.staticClass = sc.join(' ');
-    v0.data.attrs = { ...(v0.data.attrs || {}), ...attrs };
-    return slot;
+    return nodes;
+
+    // if (nodes.length !== 1) {
+    //   throw new Error('mdc-top-app-bar-icon expects exactly one child element');
+    // }
+
+    // const [v0] = nodes;
+    // const data = v0.data || (v0.data = {});
+    // const sc = [data.staticClass || ''];
+
+    // actionItem && sc.push(cssClasses.ACTION_ITEM);
+    // navIcon && sc.push(cssClasses.NAV_ICON);
+    // v0.data.staticClass = sc.join(' ');
+    // v0.data.attrs = { ...(v0.data.attrs || {}), ...attrs };
+    // return slot;
   },
 };
