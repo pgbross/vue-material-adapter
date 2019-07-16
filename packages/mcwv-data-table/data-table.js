1cb65ef8cb9832fe33050e5114e3c64ccba29eae
philip ross <philip@satchel.scot>
2019-07-16 13:53:55 +0100

    feat: working on data-table

--- packages/mcwv-data-table/data-table.js
+++ packages/mcwv-data-table/data-table.js
@@ -1,5 +1,18 @@
+import MDCDataTableFoundation from '@material/data-table/foundation';
+
 export default {
   name: 'mcw-data-table',
+  data() {},
+  mounted() {
+    const adapter = {};
+
+    this.foundation = new MDCDataTableFoundation(adapter);
+    this.foundation.init();
+  },
+  beforeDestroy() {
+    this.foundation.destroy();
+  },
+
   render(createElement) {
     const { $scopedSlots: scopedSlots } = this;
 
