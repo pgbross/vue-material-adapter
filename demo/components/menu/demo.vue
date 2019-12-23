998e77cb281b7b81baa0c1ef48ef42f1a0f7748e
philip ross <philip@satchel.scot>
2019-12-23 10:42:39 +0000

    fix: menu demo

--- demo/components/menu/demo.vue
+++ demo/components/menu/demo.vue
@@ -1,13 +1,47 @@
 <template>
-  <div class="mcw-demo mcw-demo--container">
+  <div class="mcw-demo mcw-demo--container mcw-demo--container-menu">
     <mcw-menu-anchor class="myAnchor">
       <mcw-button raised @click="open = true">Open Menu</mcw-button>
       <mcw-menu v-model="open" @select="onSelect">
-        <mcw-list-item>A Menu Item</mcw-list-item>
-        <mcw-list-item>Another Menu Item</mcw-list-item>
-        <mcw-list-item>Next Menu Item</mcw-list-item>
+        <li class="mdc-list-item" role="menuitem">
+          <span class="mdc-list-item__text">A Menu Item</span>
+        </li>
+        <li class="mdc-list-item" role="menuitem">
+          <span class="mdc-list-item__text">Another Menu Item</span>
+        </li>
         <mcw-list-divider></mcw-list-divider>
-        <mcw-list-item>Parted Menu Item</mcw-list-item>
+        <li class="mdc-list-item" role="menuitem">
+          <span class="mdc-list-item__text">A Menu Item</span>
+        </li>
+        <li class="mdc-list-item" role="menuitem">
+          <span class="mdc-list-item__text">Parted Menu Item</span>
+        </li>
+      </mcw-menu>
+    </mcw-menu-anchor>
+
+    <mcw-menu-anchor class="myAnchor">
+      <mcw-button raised @click="openGroup = true">Group Menu</mcw-button>
+      <mcw-menu v-model="openGroup" @select="onSelect">
+        <li>
+          <ul class="mdc-menu__selection-group">
+            <li class="mdc-list-item" role="menuitem">
+              <span class="mdc-list-item__graphic mdc-menu__selection-group-icon">
+                <i class="material-icons">check</i>
+              </span>
+              <span class="mdc-list-item__text">Single</span>
+            </li>
+            <li class="mdc-list-item" role="menuitem">
+              <span class="mdc-list-item__graphic mdc-menu__selection-group-icon">
+                <i class="material-icons">check</i>
+              </span>
+              <span class="mdc-list-item__text">1.15</span>
+            </li>
+          </ul>
+        </li>
+        <li class="mdc-list-divider" role="separator"></li>
+        <li class="mdc-list-item" role="menuitem">
+          <span class="mdc-list-item__text">Add space before paragraph</span>
+        </li>
       </mcw-menu>
     </mcw-menu-anchor>
   </div>
@@ -18,6 +52,7 @@ export default {
   data() {
     return {
       open: false,
+      openGroup: false,
     };
   },
 
@@ -28,3 +63,12 @@ export default {
   },
 };
 </script>
+
+<style>
+.demo-article .mcw-layout-grid {
+  width: 100%;
+}
+.demo-article .mcw-demo--container-menu .mdc-menu-anchor + .mdc-menu-anchor {
+  margin-left: 3em;
+}
+</style>
