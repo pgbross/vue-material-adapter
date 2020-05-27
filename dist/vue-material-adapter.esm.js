3f898471b4406045fb9b512e7fd5568cc08c46b0
philip ross <philip@satchel.scot>
2020-05-27 17:19:16 +0100

    chore: build

--- dist/vue-material-adapter.esm.js
+++ dist/vue-material-adapter.esm.js
@@ -2,6 +2,7 @@ import { applyPassive } from '@material/dom/events';
 import { matches, closest } from '@material/dom/ponyfill';
 import { MDCRippleFoundation } from '@material/ripple';
 import { supportsCssVariables } from '@material/ripple/util';
+import { MDCCircularProgressFoundation } from '@material/circular-progress/foundation';
 import { getCorrectEventName } from '@material/animation';
 import { MDCCheckboxFoundation } from '@material/checkbox/foundation';
 import { MDCFormFieldFoundation } from '@material/form-field/foundation';
@@ -706,6 +707,201 @@ var card = BasePlugin({
   mcwCardActions,
   mcwCardActionButtons,
   mcwCardActionIcons,
+});
+
+const ProgressPropType = {
+  type: [Number, String],
+  validator(value) {
+    return Number(value) >= 0 && Number(value) <= 1;
+  },
+};
+
+function getCircleAttrs(medium = false, indeterminate = true) {
+  return medium
+    ? {
+        cx: '16',
+        cy: '16',
+        r: '12.5',
+        'stroke-dasharray': '78.54',
+        'stroke-dashoffset': indeterminate ? '39.27' : '78.54',
+      }
+    : {
+        cx: '24',
+        cy: '24',
+        r: '18',
+        'stroke-dasharray': '113.097',
+        'stroke-dashoffset': indeterminate ? '56.549' : '113.097',
+      };
+}
+
+var mcwCircularProgress = {
+  name: 'mcw-circular-progress',
+  props: {
+    open: { type: Boolean, default: true },
+    indeterminate: Boolean,
+    medium: Boolean,
+    progress: ProgressPropType,
+    tag: { type: String, default: 'div' },
+  },
+  data() {
+    return {
+      classes: {
+        'mdc-circular-progress': 1,
+        'mdc-circular-progress--medium': this.medium,
+        'mdc-circular-progress--large': !this.medium,
+      },
+      rootAttrs: {},
+      circleAttrs: getCircleAttrs(this.medium, false),
+      indeterminateAttrs: getCircleAttrs(this.medium, true),
+    };
+  },
+  watch: {
+    open(nv) {
+      if (nv) {
+        this.foundation.open();
+      } else {
+        this.foundation.close();
+      }
+    },
+    progress(nv) {
+      this.foundation.setProgress(Number(nv));
+    },
+
+    indeterminate(nv) {
+      this.foundation.setDeterminate(!nv);
+    },
+  },
+
+  mounted() {
+    const adapter = {
+      addClass: className => {
+        this.$set(this.classes, className, true);
+      },
+
+      getDeterminateCircleAttribute: attributeName => {
+        return this.circleAttrs[attributeName];
+      },
+
+      hasClass: className => this.$el.classList.contains(className),
+      removeClass: className => this.$delete(this.classes, className),
+
+      removeAttribute: attributeName => {
+        this.$delete(this.rootAttrs, attributeName);
+      },
+
+      setAttribute: (attributeName, value) => {
+        this.$set(this.rootAttrs, attributeName, value);
+      },
+
+      setDeterminateCircleAttribute: (attributeName, value) =>
+        this.$set(this.circleAttrs, attributeName, value),
+    };
+
+    this.foundation = new MDCCircularProgressFoundation(adapter);
+    this.foundation.init();
+
+    this.foundation.setProgress(Number(this.progress));
+    this.foundation.setDeterminate(!this.indeterminate);
+
+    if (this.open) {
+      this.foundation.open();
+    } else {
+      this.foundation.close();
+    }
+  },
+  beforeDestroy() {
+    this.foundation.destroy();
+  },
+
+  methods: {
+    renderCircle(createElement, mode) {
+      const svgClass_ = {
+        determinate: 'mdc-circular-progress__determinate-circle-graphic',
+        indeterminate: 'mdc-circular-progress__indeterminate-circle-graphic',
+      };
+
+      const circleOptions = {
+        attrs:
+          mode === 'determinate' ? this.circleAttrs : this.indeterminateAttrs,
+      };
+
+      if (mode === 'determinate') {
+        circleOptions.class = 'mdc-circular-progress__determinate-circle';
+      }
+
+      return createElement(
+        'svg',
+        {
+          attrs: {
+            viewbox: '0 0 48 48',
+            xmlns: '"http://www.w3.org/2000/svg"',
+          },
+          class: svgClass_[mode],
+        },
+        [createElement('circle', circleOptions)],
+      );
+    },
+  },
+
+  render(createElement) {
+    const determinateNode = createElement(
+      'div',
+      { class: 'mdc-circular-progress__determinate-container' },
+      [this.renderCircle(createElement, 'determinate')],
+    );
+
+    const indeterminateNode = createElement(
+      'div',
+      {
+        class: 'mdc-circular-progress__indeterminate-container',
+        // domProps: { innerHTML: indeterminateHtml_ },
+      },
+      [
+        createElement(
+          'div',
+          { class: 'mdc-circular-progress__spinner-layer' },
+          [
+            createElement(
+              'div',
+              {
+                class:
+                  'mdc-circular-progress__circle-clipper mdc-circular-progress__circle-left',
+              },
+              [this.renderCircle(createElement, 'indeterminate')],
+            ),
+            createElement(
+              'div',
+              {
+                class: 'mdc-circular-progress__gap-patch',
+              },
+              [this.renderCircle(createElement, 'indeterminate')],
+            ),
+            createElement(
+              'div',
+              {
+                class:
+                  'mdc-circular-progress__circle-clipper mdc-circular-progress__circle-right',
+              },
+              [this.renderCircle(createElement, 'indeterminate')],
+            ),
+          ],
+        ),
+      ],
+    );
+
+    return createElement(
+      this.tag,
+      {
+        class: this.classes,
+        attrs: { role: 'progressbar', ...this.rootAttrs },
+      },
+      [determinateNode, indeterminateNode],
+    );
+  },
+};
+
+var circularProgress = BasePlugin({
+  mcwCircularProgress,
 });
 
 /* eslint-disable quote-props */
@@ -2989,7 +3185,7 @@ var lineRipple = BasePlugin({
   mcwLineRipple,
 });
 
-const ProgressPropType = {
+const ProgressPropType$1 = {
   type: [Number, String],
   validator(value) {
     return Number(value) >= 0 && Number(value) <= 1;
@@ -3002,8 +3198,8 @@ var mcwLinearProgress = {
     open: { type: Boolean, default: true },
     indeterminate: Boolean,
     reversed: Boolean,
-    progress: ProgressPropType,
-    buffer: ProgressPropType,
+    progress: ProgressPropType$1,
+    buffer: ProgressPropType$1,
     bufferingDots: { type: Boolean, default: true },
     tag: { type: String, default: 'div' },
   },
@@ -7317,6 +7513,7 @@ var index$1 = {
   install(vm) {
     vm.use(button);
     vm.use(card);
+    vm.use(circularProgress);
     vm.use(checkbox);
     vm.use(chips);
     vm.use(dataTable);
