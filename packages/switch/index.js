89ca5dcc513262089de14738bd18d769eb32edbf
philip ross <philip@satchel.scot>
2020-06-22 11:15:47 +0100

    feat: update switch to SFC

--- packages/switch/index.js
+++ packages/switch/index.js
@@ -1,5 +1,5 @@
 import { BasePlugin } from '~/base/index.js';
-import mcwSwitch from './switch.js';
+import mcwSwitch from './switch.vue';
 
 export { mcwSwitch };
 
---
+++ packages/switch/switch.vue
@@ -0,0 +1,2 @@
+<script src="./switch.js"></script>
+<template src="./switch.html"></template>
