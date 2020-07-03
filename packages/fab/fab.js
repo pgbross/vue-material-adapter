824bf54d002c7025d5a911ff0325bac03daba046
philip ross <philip@satchel.scot>
2020-07-03 11:30:24 +0100

    feat: fab composition

--- packages/fab/fab.js
+++ packages/fab/fab.js
@@ -1,34 +1,61 @@
-import { CustomButtonMixin, DispatchEventMixin } from '~/base/index.js';
-import { RippleMixin } from '~/ripple/index.js';
+import { computed, reactive, ref, toRefs, watch } from '@vue/composition-api';
+import { useRipplePlugin } from '~/ripple/ripple-plugin';
+
+import {
+  CustomButton,
+  customButtonProps,
+  useCutomButtonPlugin,
+} from '~/base/index.js';
 
 export default {
   name: 'mcw-fab',
-  mixins: [DispatchEventMixin, CustomButtonMixin, RippleMixin],
   props: {
+    ...customButtonProps,
     icon: String,
     mini: Boolean,
     exited: Boolean,
     label: String,
   },
-  data() {
-    return {
+  components: { CustomButton },
+  setup(props, { slots, listeners }) {
+    const root = ref(null);
+    const uiState = reactive({
       classes: {
         'mdc-fab': 1,
-        'mdc-fab--mini': this.mini,
-        'mdc-fab--extended': this.label || this.$slots.default,
-        'mdc-fab--exited': this.exited,
+        'mdc-fab--mini': props.mini,
+        'mdc-fab--extended': props.label || slots.default,
+        'mdc-fab--exited': props.exited,
       },
-    };
-  },
-  watch: {
-    icon() {
-      this.$set(this.classes, 'material-icons', this.icon);
-    },
-    mini() {
-      this.$set(this.classes, 'mdc-fab--mini', this.mini);
-    },
-    exited() {
-      this.$set(this.classes, 'mdc-fab--exited', this.exited);
-    },
+    });
+
+    const { link } = useCutomButtonPlugin(props);
+
+    const { classes: rippleClasses, styles } = useRipplePlugin(root);
+    const classes = computed(() => {
+      return { ...rippleClasses.value, ...uiState.classes };
+    });
+
+    watch(
+      () => props.icon,
+      nv => {
+        uiState.classes = { ...uiState.classes, 'material-icons': nv };
+      },
+    );
+
+    watch(
+      () => props.mini,
+      nv => {
+        uiState.classes = { ...uiState.classes, 'mdc-fab--mini': nv };
+      },
+    );
+
+    watch(
+      () => props.exited,
+      nv => {
+        uiState.classes = { ...uiState.classes, 'mdc-fab--exited': nv };
+      },
+    );
+
+    return { ...toRefs(uiState), classes, root, styles, link, listeners };
   },
 };
