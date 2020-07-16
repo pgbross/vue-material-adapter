7e880844ac43c573a5ab584b667547ef6bf95bf9
philip ross <philip@satchel.scot>
2020-07-16 18:07:17 +0100

    feat: show svg icon dynamically loaded  in button demo

--- demo/components/button/demo.vue
+++ demo/components/button/demo.vue
@@ -12,6 +12,20 @@
         <mcw-button v-bind="buttonProps" trailing-icon="done"
           >Button</mcw-button
         >
+        <mcw-button v-bind="buttonProps"
+          ><template #trailingIcon
+            ><i class="material-icons mdc-button__icon" aria-hidden="true">{{
+              icon
+            }}</i></template
+          >
+          Button</mcw-button
+        >
+        <mcw-button v-bind="buttonProps"
+          ><template #trailingIcon
+            ><html-fragment :html="svg"></html-fragment>
+          </template>
+          Button</mcw-button
+        >
       </div>
     </div>
 
@@ -56,11 +70,35 @@
 </template>
 
 <script>
+/* eslint-disable max-len */
+
+import Vue from 'vue';
+const next_ = `<svg class="mdc-button__icon" version="1.1" viewBox="0 0 53.333 53.333" xmlns="http://www.w3.org/2000/svg">
+ <path d="m26.601 2e-3c-14.728 0-26.665 11.938-26.665 26.665 0 14.728 11.938 26.667 26.665 26.667 14.728 0 26.667-11.94 26.667-26.667 0-14.728-11.94-26.665-26.667-26.665zm9.3143 15.826h3.328v21.677h-3.328zm-14.012 4.3827 11.186 6.4568-11.186 6.4588v-4.7909h-11.535v-3.3339h11.535z" fill="var(--next-color)" stroke-width="1.3333"/>
+</svg>`;
+
+// helper to render an html fragment without a parent node
+Vue.component('html-fragment', {
+  functional: true,
+  props: { html: String },
+  render(h, ctx) {
+    const nodes = new Vue({
+      beforeCreate() {
+        this.$createElement = h;
+      }, // not necessary, but cleaner imho
+      template: `<div>${ctx.props.html}</div>`,
+    }).$mount()._vnode.children;
+    return nodes;
+  },
+});
+
 export default {
   data() {
     return {
       type: '',
       disabled: false,
+      icon: 'favorite',
+      svg: '',
     };
   },
   computed: {
@@ -81,6 +119,18 @@ export default {
         unelevated: this.unelevated,
       };
     },
+    getsvg() {
+      return this.svg;
+    },
+  },
+  mounted() {
+    setTimeout(() => {
+      this.icon = 'flight_takeoff';
+    }, 2000);
+
+    setTimeout(() => {
+      this.svg = next_;
+    }, 1000);
   },
   methods: {
     onClick() {
