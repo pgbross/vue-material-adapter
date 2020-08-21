a776b8dcbe6b206d93ebc9e0ef79ab91e9cc6100
philip ross <philip@satchel.scot>
2020-08-21 17:47:08 +0100

    feat: update basic-vue-cli for 0.17.0

--- examples/basic-vue-cli/src/main.js
+++ examples/basic-vue-cli/src/main.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueMaterialAdapter from "vue-material-adapter";
+import VueCompositionAPI from '@vue/composition-api';
+import Vue from 'vue';
+import VueMaterialAdapter from 'vue-material-adapter';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.use(VueCompositionAPI);
+
+Vue.config.productionTip = false;
 Vue.use(VueMaterialAdapter);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
