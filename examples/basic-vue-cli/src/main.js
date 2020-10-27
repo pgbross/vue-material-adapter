b53b02bbc4adfb23b710aee0ba4943fedc5619cf
philip ross <philip@satchel.scot>
2020-10-27 10:32:16 +0000

    fix: remove vue-router dependency in basic example

--- examples/basic-vue-cli/src/main.js
+++ examples/basic-vue-cli/src/main.js
@@ -1,19 +1,12 @@
 import { createApp, h } from 'vue';
 import VueMaterialAdapter from 'vue-material-adapter';
 import App from './App.vue';
-import { createRouter, createWebHashHistory } from 'vue-router';
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes: [],
-});
 
 // mount app
 const app = createApp({
   render: () => h(App),
 });
 
-app.use(router);
 app.use(VueMaterialAdapter);
 
 app.mount('#app');
