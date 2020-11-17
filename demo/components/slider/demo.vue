b7550e02593937a15d441930eb34acfc5d3781b2
philip ross <philip@satchel.scot>
2020-11-17 09:45:15 +0000

    feat: update slider for v8

--- demo/components/slider/demo.vue
+++ demo/components/slider/demo.vue
@@ -2,19 +2,28 @@
   <div class="mcw-demo mcw-demo--container">
     <div class="slider-demo">
       <div class="mdc-typography--caption">Continuous</div>
-      <mcw-slider v-model="sliderValue1" max="10" />
-      <div class="mdc-typography--caption">Continuous Rtl</div>
-      <mcw-slider v-model="sliderValue1" max="10" dir="rtl" />
-      <div class="mdc-typography--caption">Discrete</div>
-      <mcw-slider v-model="sliderValue1" discrete max="10" />
-      <div class="mdc-typography--caption">Discreete with markers</div>
+      <!-- <mcw-slider v-model="sliderValue1" max="10" /> -->
+
+      <div class="mdc-typography--caption">Continuous range</div>
       <mcw-slider
+        v-model="sliderValue1"
+        v-model:start="sliderValueStart"
+        max="10"
+        range
+      />
+
+      <div class="mdc-typography--caption">Continuous Rtl</div>
+      <!-- <mcw-slider v-model="sliderValue1" max="10" dir="rtl" /> -->
+      <div class="mdc-typography--caption">Discrete</div>
+      <!-- <mcw-slider v-model="sliderValue1" discrete max="10" data-step="1" /> -->
+      <div class="mdc-typography--caption">Discreete with markers</div>
+      <!-- <mcw-slider
         v-model="sliderValue2"
         discrete
-        step="2"
+        data-step="2"
         max="10"
-        display-markers
-      />
+        tick-marks
+      /> -->
     </div>
   </div>
 </template>
@@ -23,8 +32,9 @@
 export default {
   data() {
     return {
+      sliderValueStart: 2,
       sliderValue1: 5,
-      sliderValue2: 5,
+      sliderValue2: 4,
     };
   },
 };
