93338cbaedefb22c939d93f9872ac2c8387e6fc5
philip ross <philip@satchel.scot>
2022-03-29 15:13:20 +0100

    feat(radio): refactor

---
+++ src/base/form-field-wrapper.js
@@ -0,0 +1,68 @@
+import { MDCFormFieldFoundation } from '@material/form-field/foundation.js';
+import { computed, h, onBeforeUnmount, onMounted, ref } from 'vue';
+
+export default {
+  props: {
+    activate: Function,
+    deactivate: Function,
+    alignEnd: Boolean,
+    forId: String,
+  },
+  inheritAttrs: false,
+  setup(props, { slots }) {
+    const labelElement = ref();
+
+    let formField;
+
+    const hasLabel = !!slots.label;
+
+    const formFieldClasses = computed(() => {
+      return {
+        'mdc-form-field': hasLabel,
+        'mdc-form-field--align-end': hasLabel && props.alignEnd,
+      };
+    });
+
+    onMounted(() => {
+      if (props.hasLabel) {
+        formField = new MDCFormFieldFoundation({
+          registerInteractionHandler: (type, handler) => {
+            labelElement.value.addEventListener(type, handler);
+          },
+          deregisterInteractionHandler: (type, handler) => {
+            labelElement.value.removeEventListener(type, handler);
+          },
+          activateInputRipple: () => {
+            props?.activate();
+          },
+          deactivateInputRipple: () => {
+            props?.deactivate();
+          },
+        });
+        formField.init();
+      }
+    });
+
+    onBeforeUnmount(() => {
+      formField?.destroy();
+    });
+
+    return () => {
+      if (hasLabel) {
+        return h('div', { class: { ...formFieldClasses.value } }, [
+          slots.default?.(),
+          h(
+            'label',
+            {
+              for: props.forId,
+              ref: labelElement,
+            },
+            [slots.label?.()],
+          ),
+        ]);
+      }
+
+      return slots.default();
+    };
+  },
+};
--- src/base/index.js
+++ src/base/index.js
@@ -1,4 +1,5 @@
 export { BasePlugin } from './base-plugin.js';
 export { emitCustomEvent } from './custom-event.js';
 export { CustomLink } from './custom-link.js';
+export { default as formFieldWrapper } from './form-field-wrapper.js';
 export { default as touchWrapper } from './touch-wrapper.js';
