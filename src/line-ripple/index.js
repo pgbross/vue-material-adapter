eea5c81b0644a0c05c49867634e6eb4ac127b339
philip ross <philip@satchel.scot>
2022-03-29 12:10:03 +0100

    feat: implement line ripple render function

--- src/line-ripple/index.js
+++ src/line-ripple/index.js
@@ -1,8 +1,7 @@
 import { BasePlugin } from '../base/index.js';
-import mcwLineRipple from './line-ripple.vue';
-
-export { mcwLineRipple };
+import mcwLineRipple from './line-ripple.js';
 
 export default BasePlugin({
   mcwLineRipple,
 });
+export { default as mcwLineRipple } from './line-ripple.js';
--- src/line-ripple/line-ripple.vue
+++
@@ -1,9 +0,0 @@
-<script src="./line-ripple.js"></script>
-<template>
-  <span
-    :class="lineClasses"
-    :style="lineStyles"
-    @transitionend="onTransitionEnd"
-  >
-  </span>
-</template>
